88b6d63e43a0ca74e25e70cced1838aa8d1e1e0c
Peter Stein <peterstein@protonmail.com>
2017-10-03 16:03:33 +0200

    slight format improvement

--- examples/example_main.cpp
+++ examples/example_main.cpp
@@ -6,16 +6,18 @@ int main() {
     using namespace confusepp;
     Root root{Option<int>("int_value").default_value(0),
               Section("string_section").values(Option<std::string>("string_identifier").default_value("test_string")),
-              Section("int_section")
-                  .values(Option<int>("int_one"), Option<int>("int_two"), Option<int>("int_three"))
-                  .title("test"),
+              Section("int_section").values(
+                              Option<int>("int_one"),
+                              Option<int>("int_two"),
+                              Option<int>("int_three")
+              ).title("test"),
               Multisection("multi").values(Option<std::string>("string_identifier")),
               Option<List<int>>("int_list").default_value(42, 13),
               Option<List<float>>("float_list"),
               Option<List<bool>>("bool_list"),
               Option<List<std::string>>("string_list").default_value("test, test", "test2")};
 
-    auto config = Config::parse_config("../../examples/test.conf", root);
+    auto config = Config::parse_config("/examples/test.conf", root);
 
     if (config) {
         std::cout << "Config is valid." << std::endl;
